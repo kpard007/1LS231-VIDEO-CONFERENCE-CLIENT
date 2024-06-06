5977463bc9facf401a339fcb6f12ef2da5daa020
hiryonz <hiryonz024@gmail.com>
2024-06-05 19:25:36 -0500

    arreglar errores de [SETUP] - Crear el archivo js/helpers/notes.js

--- js/helpers/notes.js
+++ js/helpers/notes.js
@@ -1,37 +1,25 @@
 
 class NotesHelper {
-     constructor () {
-        this.notes = [];
-    }
+ 
+    static notes = [];
 
-    add(note) {
+
+    static add(note) {
         const newId = "12345";
-        const newNote = {id: newId, note};
-        this.notes.push(newNote);
-        return newID;
+        return newId;
     }
     
-    delete(noteId) {
-        const index = this.notes.findIndex(note => note.id === noteId);
-        if (index !== -1) {
-            this.notes.splice(index, 1);
-            return true;
-        }
-        return false;
+    static delete(noteId) {
+        return true;
     }
 
 
-    getList(roomId) {
-        return this.notes.filter(note => note.roomCode === roomId)
+    static getList(roomCode) {
+        return Note;
     }
 
-    editNote(noteId, note) {
-        const index = this.notes.findIndex(note => note.id === noteId);
-        if(index !== -1){
-            this.notes[index] = {id: noteId, note};
-            return true;
-        }
-        return false;
+    static editNote(noteId, note) {
+        return true;
     }
 
 }
