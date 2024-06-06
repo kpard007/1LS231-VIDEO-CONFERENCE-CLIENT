541b1ef8ac0a4142c4c95aef7977c3544171d525
Gabriel A. Garci - 8-883-2158 <gabriel.garcia@utp.ac.pa>
2024-06-05 20:00:36 -0500

    mod. onStream removed

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ document.addEventListener('alpine:init', () => {
                     this.streamList.push(streamInfo);
                 },
                 (stream) => {
-                    this.streamList = this.streamList.filter(x => x.streamId != stream.streamId);
+                    this.streamList = this.streamList.filter(x => x.streamInfo.streamId != stream.streamId);
                 }
             );            
         },
