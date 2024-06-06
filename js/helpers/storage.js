60a0c90a1928c2f8005b1a5a998ca469fe868730
AngelisNG <103153224+AngelisNG@users.noreply.github.com>
2024-06-05 19:27:30 -0500

    task qzIuvoob

---
+++ js/helpers/storage.js
@@ -0,0 +1,18 @@
+class StorageHelper{
+    static getFiles (path){
+        return [
+            `uploads/file1.txt`,
+            `uploads/file2.jpg`,
+            `uploads/file3.pdf`
+        ];
+    }
+
+    static upload(file,path,progress_callback){
+        return true; 
+    }
+
+    static download(file, path) {  
+        return "https://google.com";
+    }
+}
+
