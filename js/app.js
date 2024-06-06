b66e877c567ee36542980ba4714e088c74eeb420
jeaAcosta <jeannine.acosta@hotmail.com>
2024-06-05 19:05:10 -0500

    task m9gMiInG

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ document.addEventListener('alpine:init', () => {
         message: "",
         view: "call",
         files: [],
+        notes:'',
         toggleMode(){
             if (this.mode == "light") {
                 this.mode = "dark";
