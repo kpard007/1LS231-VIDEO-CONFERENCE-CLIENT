3b709522aa8186e41f3d61f10165f43bf40aaf0f
jeaAcosta <jeannine.acosta@hotmail.com>
2024-06-05 19:01:14 -0500

    task tJsGiFQc

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ document.addEventListener('alpine:init', () => {
         chats: [],
         message: "",
         view: "call",
+        files: [],
         toggleMode(){
             if (this.mode == "light") {
                 this.mode = "dark";
